529f41726d7b6ede324796a5a8396324ec75e766
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2024-01-27 23:33:37 +0530

    setting up

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <main>
+    <header>
+      <a href="" className ="logo">MyBlog</a>
+     <nav>
+      <a href ="">Login</a>
+      <a href ="">Register</a>
+     </nav>
+
+    </header>
+    
+      <div className="post">
+       
+        <img src="https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?cs=srgb&dl=bloom-blooming-blossom-462118.jpg&fm=jpg" alt='img'/>
+        <div className="text">
+     <h2>Biodiversity: Big Picture and Backyard</h2>
+      <p>There are big issues and topics that we’ll continue to have to deal with in the upcoming months of 2024, and rapid biodiversity loss in this sixth mass extinction is one of them.
+
+Every year that we fail to stem the tide, climate change gets closer to abruptly pushing species over tipping points as their geographic ranges reach higher and higher temperatures. That’s the big picture, and it’s some heavy stuff.
+</p>
+</div>
+      </div>
+      
+      <div className="post">
+
+        <img src="https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?cs=srgb&dl=bloom-blooming-blossom-462118.jpg&fm=jpg" alt='img'/>
+        <div className="text">
+     <h2>Biodiversity: Big Picture and Backyard</h2>
+      <p>There are big issues and topics that we’ll continue to have to deal with in the upcoming months of 2024, and rapid biodiversity loss in this sixth mass extinction is one of them.
+
+Every year that we fail to stem the tide, climate change gets closer to abruptly pushing species over tipping points as their geographic ranges reach higher and higher temperatures. That’s the big picture, and it’s some heavy stuff.
+</p>
+</div>
+      </div>
+      
+      <div className="post">
+       
+        <img src="https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?cs=srgb&dl=bloom-blooming-blossom-462118.jpg&fm=jpg" alt='img'/>
+        <div className="text">
+     <h2>Biodiversity: Big Picture and Backyard</h2>
+      <p>There are big issues and topics that we’ll continue to have to deal with in the upcoming months of 2024, and rapid biodiversity loss in this sixth mass extinction is one of them.
+
+Every year that we fail to stem the tide, climate change gets closer to abruptly pushing species over tipping points as their geographic ranges reach higher and higher temperatures. That’s the big picture, and it’s some heavy stuff.
+</p>
+</div>
+      </div>
+
+
+
+   </main>
+  );
+}
+
+export default App;
